5e37f60d556623551819512e9492135559a18abf
Sachin Raj <sachinraj6392@gmail.com>
2024-11-15 16:22:09 +0530

    Improve css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,55 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from '../CSS/Skills.module.css';
 import { fetchData } from '../ApiFetch/FetchApi';
 import Loading from '../Loading.jsx/Loading';
 
 function Skills() {
-
   const [skills, setSkills] = useState(null);
   const [error, setError] = useState(null);
+  const [hasFetched, setHasFetched] = useState(false);
+  const skillsSectionRef = useRef(null);
 
   useEffect(() => {
-    async function fetchInfoData() {
-      try {
-        const endpoint = 'skills';
-        const result = await fetchData(endpoint);
-        console.log('result = ', result);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !hasFetched) {
+          fetchSkillsData();
+        }
+      },
+      {
+        root: null,
+        threshold: 0.1,
+      }
+    );
 
-        setTimeout(() => {
-          setSkills(result);
-        }, 1000);
+    if (skillsSectionRef.current) {
+      observer.observe(skillsSectionRef.current);
+    }
 
-      } catch (err) {
-        setError(err.message);
+    return () => {
+      if (skillsSectionRef.current) {
+        console.log("unobserve");
+
+        observer.unobserve(skillsSectionRef.current);
       }
     };
+  }, [hasFetched]); 
 
-    fetchInfoData();
+  const fetchSkillsData = async () => {
+    try {
+      setHasFetched(true); 
+      const endpoint = 'skills';
+      const result = await fetchData(endpoint);
+      console.log('result = ', result);
 
-  }, []);
+      setTimeout(() => {
+        setSkills(result);
+      }, 1000);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   return (
-    <section className={styles.about_skills} id='skills'>
+    <section className={styles.about_skills} id="skills" ref={skillsSectionRef}>
       <div className={styles.skills_heading}>About skills</div>
       {skills ? (
         <div className={styles.skills_container}>
-          {
-            skills.map((ele, index) => (
-              <div className={styles.skill_card} key={index}>
-
-                <div className={styles.skill_img_div}>
-                  <img
-                    className={styles.skill_img}
-                    src={ele.skills_logo ? ele.skills_logo : "image"}
-                    alt={ele.skills_logo ? "Skill logo" : "Placeholder image"}
-                  />
-                </div>
-
-                <h3 className={styles.skill_names}>{ele.skills}</h3>
+          {skills.map((ele, index) => (
+            <div className={styles.skill_card} key={index}>
+              <div className={styles.skill_img_div}>
+                <img className={styles.skill_img}
+                  src={ele.skills_logo ? ele.skills_logo : "image"}
+                  alt="Skill logo"
+                />
               </div>
-            ))
-          }
-
+              <h3 className={styles.skill_names}>{ele.skills}</h3>
+            </div>
+          ))}
         </div>
       ) : error ? (
         <div style={{ color: 'var(--text-color)' }}>{error}</div>
